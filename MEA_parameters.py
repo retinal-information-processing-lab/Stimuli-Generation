97d8256c68f137ceaa0c1c8da53db221f3516e4d
Sarah Goethals <sarah.goethals@hotmail.fr>
2022-02-03 12:02:01 +0100

    Create MEA_parameters.py

---
+++ MEA_parameters.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+This script contains the MEA parameters.
+
+stimulus_generation.py calls the parameters corresponding to the chosen MEA.
+
+"""
+
+mea_1_params = {
+            "pixel_size": 2.3, # um
+            # "dmd_x_dimension": , # um
+            # "dmd_y_dimension": , # um
+            }
+
+mea_2_params = {
+            "pixel_size": 3.5, # um
+            "dmd_x_dimension": 1920, # um
+            "dmd_y_dimension": 1080, # um
+            }
+
+mea_2_params = {
+            "pixel_size": 3.5, # um
+            "dmd_x_dimension": 1920, # um
+            "dmd_y_dimension": 1080, # um
+            }
+
+   
+    
