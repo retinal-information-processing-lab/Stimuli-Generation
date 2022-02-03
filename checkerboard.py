ad4865ed107d952c58c2bff5c69a4431c0cbcead
Sarah Goethals <sarah.goethals@hotmail.fr>
2022-02-03 15:00:33 +0100

    commented code

--- checkerboard.py
+++ checkerboard.py
@@ -1,10 +1,17 @@
+"""
 
+A class to build the checkerboard stimulus.
+
+Args:
+    - nb_checks: the number of checks
+    - binary_source_path: path to the binary source file
+    - rig_nb: the set-up used for the experiments
+
+"""
 
 import os
 from tqdm import tqdm
 import numpy as np
-# import matplotlib.gridspec as gds
-# import matplotlib.pyplot as plt
 import pickle
 
 class Checkerboard:
@@ -42,6 +49,26 @@ class Checkerboard:
         return shape
 
     def get_image(self, image_nb):
+        """
+        
+        Create one checkerboard frame.
+
+        Parameters
+        ----------
+        image_nb : int
+            The frame number.
+
+        Raises
+        ------
+        ValueError
+            Bit value is not 0 or 1.
+
+        Returns
+        -------
+        image : array
+            Array containing 0 and 1 of dimension nb_checks x nb_checks.
+
+        """
 
         shape = self.get_image_shape()
         image = np.zeros(shape, dtype=np.float)
@@ -50,7 +77,6 @@ class Checkerboard:
             for j in range(0, self._nb_checks):
                 bit_nb = (self._nb_checks * self._nb_checks * image_nb) + (self._nb_checks * i) + j
                 bit = self._get_bit(bit_nb)
-                # Here modifications were made on 5.01.2021 by TBT to get the same polarity as the one seen on camera
                 if bit == 0:
                     image[i, j] = 0.0
                 elif bit == 1:
@@ -69,6 +95,21 @@ class Checkerboard:
         return image
     
     def build_checkerboard(self, nb_frames):
+        """
+        
+        Creates the total checkerboard with nb_frames images.
+
+        Parameters
+        ----------
+        nb_frames : int
+            The total number of different checkerboard in the stimulation.
+
+        Returns
+        -------
+        checkerboard_array : array of dimension nb_checks x nb_checks x nb_frames 
+            An array containing all the frames.
+
+        """
         checkerboard_array = np.ones((nb_frames,self._nb_checks,self._nb_checks), dtype='uint8') 
         for i in tqdm(range(checkerboard_array.shape[0]), desc="Reconstruction the stimulus"):
             checkerboard_array[i,:,:] = self.get_image(i)
